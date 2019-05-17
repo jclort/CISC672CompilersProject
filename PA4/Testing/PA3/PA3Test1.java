9a26fcbdbbdae611a92b6f5405bf83f55e623e12
Christian <christian@ubu.localdomain>
2019-05-17 15:29:29 -0700

    Updated code.

--- PA4/Testing/PA3/PA3Test1.java
+++
@@ -1,7 +0,0 @@
-import meggy.Meggy;
-
-class PA3Test1 {
-	public static void main(String[] args) {
-		Meggy.setPixel((byte)((byte)1 + (byte)1), (byte)1, Meggy.Color.BLUE);
-	}
-}
--- PA4/Testing/PA3/PA3Test2.java
+++
@@ -1,9 +0,0 @@
-import meggy.Meggy;
-
-class PA3Test2 {
-	public static void main(String[] args) {
-		while (5 == 5) {
-			Meggy.setPixel((byte)1, (byte)2, Meggy.Color.BLUE);
-		} 
-	}
-}
